f0549ef3d18574c416dc58a2968f968669f50050
Abhishek-S-Lal <abhishekslal@gmail.com>
2020-04-27 17:45:13 +0530

    Initial commit

---
+++ app/src/main/java/com/eduzap/android/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.eduzap.android;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.eduzap.android.MainActivity;
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
